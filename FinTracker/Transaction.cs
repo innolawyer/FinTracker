ba54d2ed636c1a2d3a02baea02c36e806c0233b5
AzarovRoman <azarovrom9215@gmail.com>
2021-12-01 00:47:33 +0300

    AddTransactionClass

---
+++ FinTracker/Transaction.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinTracker
+{
+    internal class Transaction
+    {
+        public double Amount;
+        public string Category;
+        public DateTime Date;
+        public string Comment;
+        public string Sign;
+
+        public Transaction(string sign, double sum, DateTime date, string comment, string category)
+        {
+            Sign = sign;
+            Amount = sum;
+            Date = date;
+            Comment = comment;
+            Category = category;
+        }
+    }
+}
