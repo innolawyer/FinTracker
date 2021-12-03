e7dc1add6a48b368f4a9c1b50043e921e7649e52
Alyona <alianakass@icloud.com>
2021-12-03 18:46:14 +0300

    Start Writing Storage

---
+++ FinTracker/Storage.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinTracker
+{
+    public sealed class Storage
+    {
+        private static Storage _instance;
+
+        public List<Asset> Assets;
+        public List<User> Users;
+        public List<Transaction> Transactions;
+        public List<string> CashbackCategories;
+
+        public Storage()
+        {
+            Assets = new List<Asset>();
+            Users = new List<User>();
+            Transactions = new List<Transaction>();
+            CashbackCategories = new List<string>();
+        }
+
+        public static Storage GetInstance()
+        {
+            if (_instance == null)
+            {
+                _instance = new Storage();
+            }
+            return _instance;
+        }
+       
+    }
+}
