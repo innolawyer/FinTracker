bcc104251ffd270cbc2a23902746a5fe6fcb2e4e
AzarovRoman <azarovrom9215@gmail.com>
2021-12-09 15:08:55 +0300

    add calcer in all asset

---
+++ FinTracker/Assets/FVCalc/CashFVCalc.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinTracker.Assets.FVCalc
+{
+    internal class CashFVCalc : IFVCalcer
+    {
+        public double GetFutureValue(double amount, int term, double interest)
+        {
+            return amount;
+        }
+    }
+}
