d0462bb2d3a844def3ced92896a0986359b95980
Artur Khamatnurov <arturkh.116@gmail.com>
2021-12-14 15:41:37 +0300

    Add EditLoan

--- FinTracker/Loans/EditLoanWindow.xaml.cs
+++ FinTracker/Loans/EditLoanWindow.xaml.cs
@@ -20,9 +20,58 @@ namespace FinTracker.Loans
     public partial class EditLoanWindow : Window
     {
         MainWindow _mainWindow;
+        Storage _storage;
         public EditLoanWindow(MainWindow mainWindow)
         {
             InitializeComponent();
+            _mainWindow = mainWindow;
+            FillingComboBoxLoanAssetEdit();
+            FillingComboboxLoanStatusEdit();
+            FillingEditLoanWindow();
+        }
+
+        public void FillingComboBoxLoanAssetEdit()
+        {
+            foreach (Asset asset in _storage.actualUser.Assets)
+            {
+                ComboBoxLoanAssetEdit.Items.Add(asset.Name);
+            }
+        }
+
+        public void FillingComboboxLoanStatusEdit()
+        {
+            ComboBoxLoanStatusEdit.Items.Add("Не выплачен");
+            ComboBoxLoanStatusEdit.Items.Add("Выплачен");
+
+        }
+
+        public void FillingEditLoanWindow()
+        {
+            
+            Loan loan = ((Loan)_mainWindow.ListViewLoans.SelectedItem);
+            DatePickerLoanStartEdit.SelectedDate = Convert.ToDateTime(loan.ActualPaymentDateTime);
+            ComboBoxLoanAssetEdit.SelectedItem = Convert.ToString(loan.Asset.Name);
+            TextBoxLoanCreditorNameEdit.Text = loan.CreditorsName;
+            TextBoxLoanPercentEdit.Text = Convert.ToString(loan.Percent);
+            TextBoxLoanPeriodEdit.Text = Convert.ToString(loan.Period);
+            TextBoxLoanAmountEdit.Text = Convert.ToString(loan.Amount);
+            ComboBoxLoanStatusEdit.SelectedItem = loan.Status;
+            TextBoxRemainingTermEdit.Text = Convert.ToString(loan.RemainingTerm);
+        }
+
+        private void ButtonEditLoan_Click(object sender, RoutedEventArgs e)
+        {
+            User user = _storage.actualUser;
+            Asset asset = user.GetAssetByName(ComboBoxLoanAssetEdit.SelectedItem.ToString());
+            Loan loan = ((Loan)_mainWindow.ListViewLoans.SelectedItem);
+            loan.ActualPaymentDateTime = Convert.ToDateTime(DatePickerLoanStartEdit.SelectedDate);
+            loan.Asset = asset;
+            loan.CreditorsName = TextBoxLoanCreditorNameEdit.Text;
+            loan.Percent = Convert.ToDouble(TextBoxLoanPercentEdit.Text);
+            loan.Period = Convert.ToDouble(TextBoxLoanPeriodEdit.Text);
+            loan.Amount = Convert.ToDouble(TextBoxLoanAmountEdit.Text);
+            loan.Status = Convert.ToString(ComboBoxLoanStatusEdit.SelectedItem);
+            loan.RemainingTerm = Convert.ToDouble(TextBoxRemainingTermEdit.Text);
         }
     }
 }
